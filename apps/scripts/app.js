1d433fe25b4c0310d8557451f29c3d95d396ab90
sandeepsingh9190@gmail.com <sandeepsingh9190@gmail.com>
2020-05-31 15:16:18 -0400

    Lazyloaded Completed

--- apps/scripts/app.js
+++ apps/scripts/app.js
@@ -1,4 +1,5 @@
 import '../styles/style.css'
+import 'lazysizes'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
