b4007eb344475b33bf61f23ba04da4ec47d0596d
sandeepsingh9190@gmail.com <sandeepsingh9190@gmail.com>
2020-06-07 18:48:48 -0400

    Console Added to cloud function

--- cloud-functions/secrete-area.js
+++ cloud-functions/secrete-area.js
@@ -1,4 +1,6 @@
-exports.handler = function(event,context, callback) {
+exports.handler = function(event, context, callback) {
+    console.log ("This is Testing" + event)
+    
     callback(null, {
         statusCode: 200,
         body: "Welcome to the super secret area."
