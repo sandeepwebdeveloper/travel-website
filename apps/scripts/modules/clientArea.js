59304e247c60c74a9b5a2828e0c1dcb762190631
sandeepsingh9190@gmail.com <sandeepsingh9190@gmail.com>
2020-06-13 23:59:21 -0400

    Changes reupload

--- apps/scripts/modules/clientArea.js
+++ apps/scripts/modules/clientArea.js
@@ -29,7 +29,7 @@ class ClientArea {
      document.body.insertAdjacentHTML('beforeend', `
         <div class="client-area">
             <div class="wrapper wrapper--medium">
-                <h2 class="section-title section-title--blue">Secret Client Area</h2>
+                <h2 class="section-title section-title--blue">Secret Client Areas</h2>
                 <form class="client-area__form" action="">
                 <input class="client-area__input" type="text" placeholder="Enter the secret phrase">
                 <button class="btn btn--orange">Submit</button>
