68a9472d231a188c95182d9de48ea362f0ed9977
sandeepsingh9190@gmail.com <sandeepsingh9190@gmail.com>
2020-06-17 16:36:06 -0400

    Again the file has been updated

--- apps/scripts/modules/ClientArea.js
+++ apps/scripts/modules/ClientArea.js
@@ -4,7 +4,7 @@ class ClientArea {
  constructor () {
      this.injectHTML()
      this.form = document.querySelector(".client-area__form")
-     this.field = document.querySelector(".client-area__field")
+     this.field = document.querySelector(".client-area__input")
      this.contentArea = document.querySelector(".client-area__content-area")
      this.events()
  }
